f7c5950d0e95b6e8a17f71ec9de882f8c262248f
root <root@vaioio>
2024-10-29 22:50:27 +0900

    トップページの追加、レイアウトファイルの修正

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Metadata } from 'next'
+import { notoSansJp } from '@/lib/fonts'
+// import '@/app/global.css'
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | EC-DENT',
+        default: 'EC-DENT',
+    },
+    description: '学生が輝ける総合ECサイト EC-DENT',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="ja">
+            <body className={notoSansJp.className}>{children}</body>
+        </html>
+    )
+}
