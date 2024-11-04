ec001ad663d7892ce6290ae6b54c71d29b638b21
daito-kawasaki <daito.wb.b@gmail.com>
2024-11-04 16:16:43 +0900

    グローバルCSSのコメントアウト解除

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Metadata } from 'next'
 import { notoSansJp } from '@/lib/fonts'
-// import '@/app/global.css'
+import '@/app/global.css'
 
 export const metadata: Metadata = {
     title: {
