f60b14912917fdf0121380737d48e0903fda13fc
maru-kazu <kazumaru3998@gmail.com>
2024-11-20 15:56:01 +0900

    header360px完了

--- frontend/src/components/common/header.tsx
+++ frontend/src/components/common/header.tsx
@@ -4,44 +4,72 @@ import { IMGPATH_BRAND, IMGPATH_HEADER } from '@/lib/common'
 import { notoSansBengali } from '@/lib/fonts'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import { poppins } from '@/lib/fonts'
 
 export default function Header() {
-    const items_center = 'text-xs bold flex items-center'
+    const textClass = `text-xs bold flex items-center hidden lg:block  ${notoSansBengali.className}` //テキスト用のクラス
+    const imageClass = 'w-32 h-auto ' //画像用のクラス
+
+    type HeaderText = {
+        text?: string
+        href: string
+        image?: string
+    }
+
+    const headerText: HeaderText[] = [
+        { text: 'news', href: '/news' },
+        { text: 'store', href: '/store' },
+        { href: '/', image: `${IMGPATH_HEADER}ec-dent-logo.svg` },
+        { text: 'brand', href: '/brand' },
+        { text: 'login', href: '/login' },
+    ]
+
     return (
         <header>
-            <nav className="flex justify-between px-[7.32vw]">
-                <Link
-                    href="/news"
-                    className={`${items_center} ${notoSansBengali.className}`}>
-                    NEWS
-                </Link>
-
-                <Link
-                    href="/store"
-                    className={`${items_center} ${notoSansBengali.className}`}>
-                    STORE
-                </Link>
-
-                <Link href="/">
-                    <Image
-                        src={`${IMGPATH_HEADER}ec-dent-logo.svg`}
-                        alt=""
-                        width={500}
-                        height={500}
-                    />
-                </Link>
-
-                <Link
-                    href="/brand"
-                    className={`${items_center} ${notoSansBengali.className}`}>
-                    BRAND
-                </Link>
-
-                <Link
-                    href="/login"
-                    className={`${items_center} ${notoSansBengali.className}`}>
-                    LOGIN
-                </Link>
+            <nav className="flex justify-between px-4 lg:px-[7.32vw]">
+                {headerText.map((value, index) => (
+                    <Link
+                        href={value.href}
+                        key={index}
+                        className="flex items-center">
+                        {value.text && (
+                            <span className={textClass}>{value.text}</span>
+                        )}
+                        {value.image && (
+                            <Image
+                                src={value.image}
+                                alt=""
+                                className={imageClass}
+                                width={500}
+                                height={500}
+                            />
+                        )}
+                    </Link>
+                ))}
+                <div id="right" className="flex gap-x-2.5 items-center">
+                    <div
+                        id="mail_icon"
+                        className="flex border border-black opacity-70 rounded-full px-[10px] py-[5px] gap-[10px]  items-center">
+                        <FontAwesomeIcon
+                            icon={faPaperPlane}
+                            size="xs"
+                            color="#212121cc"
+                        />
+                        <span
+                            className={`${poppins.className} text-xs opacity-70`}>
+                            CONTACT
+                        </span>
+                    </div>
+                    <div
+                        id="hamburger"
+                        className=" w-[40px] h-[40px] flex flex-col gap-1 justify-center items-center border border-black rounded-[50%] ">
+                        <span className=" w-5 h-[1px] bg-black"></span>
+                        <span className=" w-5 h-[1px] bg-black"></span>
+                        <span className=" w-5 h-[1px] bg-black"></span>
+                    </div>
+                </div>
             </nav>
         </header>
     )
