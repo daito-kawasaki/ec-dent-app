0b0e49b4a15425a156e7e1d119abf73922f1cfe3
daito-kawasaki <daito.wb.b@gmail.com>
2024-11-25 16:19:57 +0900

    section-ttlにpalyfardisplayを適応

--- frontend/src/components/ui/section-ttl.tsx
+++ frontend/src/components/ui/section-ttl.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { playfairDisplay } from '@/lib/fonts'
 
 interface ttlProps {
     ttl: string
@@ -9,7 +10,8 @@ export default function SectionTitle({ ttl, sub_ttl }) {
         <div
             id="section_ttl"
             className=" w-fit pt-10 pl-7.5 pr-9 border-b border-black lg:~lg/xl:~pt-14/20 md:~md/xl:~pr-9/24.5">
-            <span className=" ~/md:~text-xs/sm block text-black opacity-70 xl:text-base">
+            <span
+                className={`~/md:~text-xs/sm block text-black opacity-70 xl:text-base ${playfairDisplay.className}`}>
                 {ttl}
             </span>
             <span className=" block pb-2 ~/md:~text-lg/xl lg:~lg/xl:~text-xl/2xl">
