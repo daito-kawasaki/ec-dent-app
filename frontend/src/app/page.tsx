83c6a9f09823552f07c6ec3188e313ed630066f8
maru-kazu <kazumaru3998@gmail.com>
2024-11-26 18:01:54 +0900

    環境構築し直しコミット

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import TopPage from '@/components/page/top-page'
 import Header from '@/components/common/header'
+import StoreHeader from '@/components/common/store-header'
 
 export default function first(second) {
     return (
--- frontend/src/components/common/header.tsx
+++ frontend/src/components/common/header.tsx
@@ -1,5 +1,86 @@
+// 'use client'
+
+// import { IMGPATH_BRAND, IMGPATH_HEADER } from '@/lib/common'
+// import { notoSansBengali } from '@/lib/fonts'
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+// import { poppins } from '@/lib/fonts'
+
+// export default function Header() {
+//     const textClass = `text-xs bold font-bold flex items-center ${notoSansBengali.className} lg:~lg/xl:~text-sm/base xl:~text-base/xl min-[112.5rem]:text-2xl` //テキスト用のクラス
+//     const imageClass = '~sp/lg:~w-32/75 h-auto' //画像用のクラス
+
+//     type HeaderText = {
+//         text?: string
+//         href: string
+//         image?: string
+//     }
+
+//     const headerText: HeaderText[] = [
+//         { text: 'news', href: '/news' },
+//         { text: 'store', href: '/store' },
+//         { href: '/', image: `${IMGPATH_HEADER}ec-dent-logo.svg` },
+//         { text: 'brand', href: '/brand' },
+//         { text: 'login', href: '/login' },
+//     ]
+
+//     return (
+//         <header>
+//             <nav className="flex items-center justify-between  ~sp/md:~px-4/10 md:~px-10/14">
+//                 {headerText.map((value, index) => (
+//                     <Link
+//                         href={value.href}
+//                         key={index}
+//                         className={value.text && ' hidden lg:block'}>
+//                         {value.text && (
+//                             <span className={textClass}>{value.text}</span>
+//                         )}
+//                         {value.image && (
+//                             <Image
+//                                 src={value.image}
+//                                 alt=""
+//                                 className={imageClass}
+//                                 width={500}
+//                                 height={500}
+//                             />
+//                         )}
+//                     </Link>
+//                 ))}
+//                 <div
+//                     id="right"
+//                     className="flex gap-x-2.5 items-center ~sp/md:~gap-5/8  lg:hidden">
+//                     <div
+//                         id="mail_icon"
+//                         className="flex border border-black opacity-70 rounded-full ~sp/lg:~px-2.5/6 ~/sm:~py-1.5/2 ~sp/lg:~gap-2.5/6 items-center">
+//                         <FontAwesomeIcon
+//                             className=" ~/sm:~text-xs/sm sm:~sm/lg:~text-sm/2xl"
+//                             icon={faPaperPlane}
+//                             size="xs"
+//                             color="#212121cc"
+//                         />
+//                         <span
+//                             className={`${poppins.className} font-medium opacity-70 ~sp/lg:~text-xs/2xl `}>
+//                             CONTACT
+//                         </span>
+//                     </div>
+//                     <div
+//                         id="hamburger"
+//                         className="~/sm:~w-7/10 ~/sm:~h-7/10 flex flex-col ~/md:~gap-0.5/1.5 justify-center items-center border border-black rounded-[50%]  sm:~sm/lg:~w-10/12  sm:~sm/lg:~h-10/12  md:w-vw-20 md:h-vw-20">
+//                         <span className="block w-vw-14 h-[2px] scale-y-50 bg-black md:w-vw-10"></span>
+//                         <span className="block w-vw-14 h-[2px] scale-y-50 bg-black md:w-vw-10"></span>
+//                         <span className="block w-vw-14 h-[2px] scale-y-50 bg-black md:w-vw-10"></span>
+//                     </div>
+//                 </div>
+//             </nav>
+//         </header>
+//     )
+// }
+
 'use client'
 
+import { useState } from 'react'
 import { IMGPATH_BRAND, IMGPATH_HEADER } from '@/lib/common'
 import { notoSansBengali } from '@/lib/fonts'
 import Image from 'next/image'
@@ -9,6 +90,13 @@ import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 import { poppins } from '@/lib/fonts'
 
 export default function Header() {
+    // ハンバーガーメニューの表示状態を管理
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState)
+    }
+
     const textClass = `text-xs bold font-bold flex items-center ${notoSansBengali.className} lg:~lg/xl:~text-sm/base xl:~text-base/xl min-[112.5rem]:text-2xl` //テキスト用のクラス
     const imageClass = '~sp/lg:~w-32/75 h-auto' //画像用のクラス
 
@@ -44,6 +132,7 @@ export default function Header() {
                                 className={imageClass}
                                 width={500}
                                 height={500}
+                                priority
                             />
                         )}
                     </Link>
@@ -67,13 +156,30 @@ export default function Header() {
                     </div>
                     <div
                         id="hamburger"
-                        className="~/sm:~w-7/10 ~/sm:~h-7/10 flex flex-col ~/md:~gap-0.5/1.5 justify-center items-center border border-black rounded-[50%]  sm:~sm/lg:~w-10/12  sm:~sm/lg:~h-10/12  md:w-vw-20 md:h-vw-20">
+                        className="~/sm:~w-7/10 ~/sm:~h-7/10 flex flex-col ~/md:~gap-0.5/1.5 justify-center items-center border border-black rounded-[50%]  sm:~sm/lg:~w-10/12  sm:~sm/lg:~h-10/12  md:w-vw-20 md:h-vw-20"
+                        onClick={toggleMenu} // ハンバーガーメニューをクリックしたときにメニューの表示/非表示を切り替え
+                    >
                         <span className="block w-vw-14 h-[2px] scale-y-50 bg-black md:w-vw-10"></span>
                         <span className="block w-vw-14 h-[2px] scale-y-50 bg-black md:w-vw-10"></span>
                         <span className="block w-vw-14 h-[2px] scale-y-50 bg-black md:w-vw-10"></span>
                     </div>
                 </div>
             </nav>
+
+            {/* ハンバーガーメニューの内容 */}
+            <div
+                className={`lg:hidden ${isMenuOpen ? 'block' : 'hidden'} mt-4`}>
+                <nav>
+                    {headerText.map((value, index) => (
+                        <Link
+                            href={value.href}
+                            key={index}
+                            className="block py-2 text-lg">
+                            {value.text}
+                        </Link>
+                    ))}
+                </nav>
+            </div>
         </header>
     )
 }
