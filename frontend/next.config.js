8b71b61e9fe1d59a8adecfc519013021ecd5d2b1
daito-kawasaki <daito.wb.b@gmail.com>
2024-12-02 16:46:10 +0900

    360px完了と、SSR、CSRエラーの解消のためのnext.configの追加、tailwind.configのカスタム値追加

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,9 @@
+const withStyledComponents = require('styled-components-nextjs')
+
+module.exports = withStyledComponents({
+    reactStrictMode: true,
+    // 他の設定
+})
 // next.config.js
 const nextConfig = {
     reactStrictMode: true,
