149b93635efc89a2e8e1892138f3a43bec2208ac
daito-kawasaki <daito.wb.b@gmail.com>
2024-12-06 16:34:50 +0900

    laravelでのデータ取得完了だが、nextで表示できない

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,11 @@
 import TopPage from '@/components/page/top-page'
 import Header from '@/components/common/header'
 
-import FetchNewsList from '@/lib/api/news/fetch-news-list'
-import NewsList from '@/components/ui/tmp'
-
 export default function first() {
     return (
         <>
             <Header />
             <TopPage />
-            <FetchNewsList />
         </>
     )
 }
