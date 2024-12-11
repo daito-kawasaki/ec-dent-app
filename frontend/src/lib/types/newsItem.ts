2011a8ccfe982acc76718b95fd1d9cdb6fd42c30
daito-kawasaki <daito.wb.b@gmail.com>
2024-12-11 14:01:18 +0900

    いらないところの削除

--- frontend/src/lib/types/newsItem.ts
+++ frontend/src/lib/types/newsItem.ts
@@ -1,22 +1,3 @@
-// export type NewsItem = Pick<
-//     NewsItemResponse,
-//     | 'news_id'
-//     | 'news_title'
-//     | 'news_category_id'
-//     | 'deleted_at'
-//     | 'updated_at'
-//     | 'created_at'
-// >
-
-// export interface NewsItemResponse {
-// news_id: number
-// news_title: string
-// news_category_id: number
-// deleted_at: any
-// updated_at: any
-// created_at: any
-// }
-
 export interface NewsItem {
     news_id: number
     news_title: string
