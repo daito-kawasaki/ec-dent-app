9a6c5f8a4b016df6b1704ca32a0d2af5202cf1ef
daito-kawasaki <daito.wb.b@gmail.com>
2024-12-12 14:26:40 +0900

    fetch部分とコンポーネント呼び出し部分の修正

--- frontend/src/components/page/top-page.tsx
+++ frontend/src/components/page/top-page.tsx
@@ -1,7 +1,12 @@
-export default function TopPage() {
-    return (
-        <main>
-            <div>トップページ </div>
-        </main>
-    )
+import { getNewsItem } from '@/lib/api/news/fetch-api'
+import { NewsItem } from '@/lib/types/newsItem'
+import NewsList from '../ui/NewsList'
+
+export default async function TopPage() {
+    const newsAllItem: NewsItem[] = await getNewsItem()
+    if (!newsAllItem) {
+        return <p>loding</p>
+    }
+
+    return <NewsList newsAllItem={newsAllItem} />
 }
